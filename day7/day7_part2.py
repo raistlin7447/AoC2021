7727decbecb3f02440bc6796861724586d4d89ff
Kris Austin <kris.austin@draft2digital.com>
2021-12-07 13:58:56 -0600

    Added alternate, more optimal solution to Day 7 Part 2

--- day7/day7_part2.py
+++ day7/day7_part2.py
@@ -1,3 +1,6 @@
+import statistics
+
+# Brute Force
 with open("day7_input.txt") as f:
     crabs = list(map(int, f.readline().strip().split(",")))
     best = 2**10000
@@ -5,4 +8,16 @@ with open("day7_input.txt") as f:
     for i in range(min(crabs), max(crabs)+1):
         total_fuel = sum(fuel(abs(crab - i)) for crab in crabs)
         best = min(best, total_fuel)
+    print(f"{best=}")
+
+# Turns out that mean is usually correct, but can sometimes vary by up to 1/2
+# https://www.reddit.com/gallery/rawxad
+with open("day7_input.txt") as f:
+    crabs = list(map(int, f.readline().strip().split(",")))
+    fuel = lambda distance: int(distance * (distance+1) / 2)
+    total_fuel = lambda x: sum(fuel(abs(crab - x)) for crab in crabs)
+    mean = statistics.mean(crabs)
+    low = int(mean - 0.5)
+    high = int(mean + 0.5)
+    best = min(total_fuel(low), total_fuel(high))
     print(f"{best=}")
